e9ab9a244cd2a9d66b9205decc0257a31ed3b394
Ana Lucia Alonso Martínez <lucia.alonsom@uanl.edu.mx>
2025-08-23 21:46:11 -0600

    #proceso y ruta

--- Procesos.ps1
+++ Procesos.ps1
@@ -1,7 +1,14 @@
+#Crea un diccionario, en el cual la clave es el # de proceso y el valor contiene una lista con el nombre del proceso y ruta. 
 $cont=1
 $procesos=Get-Process
+$dicc = New-Object System.Collections.Generic.Dictionary"[Int,String]" 
 
-$procesos | ForEach-Object{ 
-    Write-Host -BackgroundColor Cyan "$($cont), $($_.Name), $($_.Path)"
+$procesos | ForEach-Object{
+    $List = New-Object System.Collections.Generic.List[string]
+    $List.Add($_.Name)
+    $List.Add($_.Path)
+    $dicc.Add($cont, $List) 
     $cont++
 }
+
+$dicc
