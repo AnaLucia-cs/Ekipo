7094e550bb43d1800f31ab4f2d57cdb2054bdaa2
anaslzr <laura.palacioss@uanl.edu.mx>
2025-09-07 22:38:58 -0600

    Update extraccion_eventos.psm1

--- extraccion_eventos.psm1
+++ extraccion_eventos.psm1
@@ -1,5 +1,7 @@
+# Activa el modo estricto para forzar buenas prácticas en PowerShell
 Set-StrictMode -Version Latest
 
+# Definición de la función principal
 function Get-ForensicEvents {
     <#
     .SYNOPSIS
@@ -31,36 +33,45 @@ function Get-ForensicEvents {
 
     [CmdletBinding()]
     param (
+    # Nombre del log a consultar: Application, System o Security
         [Parameter(Mandatory=$true)]
         [ValidateSet("Application","System","Security")]
         [string]$LogName,
-
+        
+         # Fecha de inicio del filtro
         [Parameter(Mandatory=$true)]
         [datetime]$StartDate,
 
+        # Fecha de fin del filtro
         [Parameter(Mandatory=$true)]
         [datetime]$EndDate,
-
+        
+        # Ruta donde se exportarán los resultados en formato CSV
         [Parameter(Mandatory=$true)]
         [string]$OutputPath,
 
+        #si se activa, solo devuelve eventos sospechosos
         [switch]$SuspiciousOnly
     )
 
     try {
         Write-Host "Extrayendo eventos del log $LogName entre $StartDate y $EndDate ..." -ForegroundColor Cyan
 
+        # Lista de IDs de eventos considerados sospechosos
         $eventIDsSospechosos = @(
             4625, 4624, 4670, 4732, 4720, 4726, 4672, # Security
             6008, 7031, 7034                         # System
         )
 
+        # Obtiene eventos crudos desde el log especificado, filtrando por rango de fechas
         $rawEvents = @(Get-WinEvent -LogName $LogName | Where-Object {
             $_.TimeCreated -ge $StartDate -and $_.TimeCreated -le $EndDate
         })
 
+         # Inicializa la lista de eventos filtrados
         $filteredEvents = @()
         if ($SuspiciousOnly) {
+        # Si se pidió solo sospechosos, se filtran por los IDs definidos
             $filteredEvents = @($rawEvents | Where-Object {
                 $eventIDsSospechosos -contains $_.Id
             })
@@ -69,6 +80,7 @@ function Get-ForensicEvents {
             $filteredEvents = $rawEvents
         }
 
+        # Selecciona solo las propiedades relevantes para exportar
         $events = $filteredEvents | Select-Object TimeCreated, Id, LevelDisplayName, ProviderName, Message
 
         if ($events) {
@@ -80,8 +92,11 @@ function Get-ForensicEvents {
         }
     }
     catch {
+        # Captura errores y los muestra en rojo
         Write-Error "Ocurrió un error: $_"
     }
 }
 
+# Expone la función como parte del módulo (para que pueda importarse y usarse externamente)
 Export-ModuleMember -Function Get-ForensicEvents
+
