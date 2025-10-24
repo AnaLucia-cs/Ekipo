8f14ebb8df17c5d7ada30746d6e3cea5b44a8ab0
Ana Lucia Alonso Martínez <lucia.alonsom@uanl.edu.mx>
2025-10-24 15:07:18 -0600

    T3 main script
    Menú, recolección info activa con nmap. Falta modularlo, registrar logs, manejo de errores, funciones pasivas, authorized.

---
+++ main.py
@@ -0,0 +1,107 @@
+import subprocess
+import sys
+import logging
+import pyautogui as py
+import nmap
+
+#Configuración logging
+logging.basicConfig(
+    filename="registroEscaneos.log",
+    encoding="utf-8",
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+#obtiene el usuario
+obtener_usuario = subprocess.run(["wsl", "whoami"], capture_output=True, text=True)
+usuario = obtener_usuario.stdout.strip()
+    
+
+host="scanme.nmap.org"
+AUTHORIZED = False
+results_path="analisis_Host.txt"
+
+
+
+verificador1 = py.confirm("""Este script realiza actividades de recolección de información y escaneo limitado de puertos al host scanme.nmap.org.
+Queda terminantemente prohibida la explotación, modificación de servicios o exfiltración de datos.
+Es necesario contar con autorización firmada del propietario del dominio para continuar.
+De no tenerlo y continuar habrá repercusiones legales.""", buttons=["Aceptar", "Salir"])
+
+if verificador1 == "Aceptar":
+    logging.warning(f"El usuario {usuario} accedió al script")
+    repeat=True
+    
+    while repeat:
+        op=input("""\nSeleccione la actividad que desea realizar
+    1. Ejecutar tareas pasivas
+    2. Ejecutar tareas activas (REQUIERE AUTORIZACIÓN)
+    3. Salir
+
+       Ingrese una opción: """)
+
+        if op=='1':
+            print("\nRealizar atividades pasivas")
+            print("\n¿Desea volver al menú?")
+            repeat=input("y/n :")
+            if repeat.strip() =="y":
+                repeat=True
+            else:
+                repeat=False
+                logging.warning(f"El usuario {usuario} salió del script.")
+                sys.exit()
+        
+        elif op=='2':
+            print(f"Verificando la conexión al host {host}...")
+            try:
+                hacer_ping = subprocess.run(["wsl", "ping", "-c", "2", host], capture_output=True, text=True)
+
+                if (hacer_ping.returncode !=0 ):
+                    print(f"El host {host} no pudo ser accedido.")
+                    sys.exit(hacer_ping.returncode)  
+            except:
+                print(f"Fallo al realizar ping al host {host}")
+                sys.exit(1)
+
+            print("Conexión exitosa")
+            try:
+                #Empezar el escaneo de puertos del host
+                print("Escaneando puertos...")
+                cuidar_puertos="21,22,23,25,3306,54,8080,8443,80,443"
+                
+                escanear = subprocess.run(["wsl", "nmap","-sV", "-p",cuidar_puertos,"--script","http-headers,http-title,ssl-cert", host], capture_output=True, text=True)
+                print(escanear.stdout)
+
+                with open(results_path, "w") as external_file:
+                    print(escanear.stdout, file=external_file)
+                    external_file.close()
+                    
+               # print(escanear.returncode)
+                print("Escaneo terminado.")
+
+            except Exception as e:
+                print("Ocurrió un error en el escaneo")
+            
+            print("\n¿Desea volver al menú?")
+            repeat=input("y/n :")
+            if repeat.strip() =="y":
+                repeat=True
+            else:
+                repeat=False
+                logging.warning(f"El usuario {usuario} salió del script.")
+                sys.exit()
+
+
+        elif op=='3':
+            logging.warning(f"El usuario {usuario} salió del script.")
+            print("Saliendo...")
+            sys.exit()
+        else:
+            print("Opción no válida.")
+
+
+else:
+    logging.warning(f"El usuario {usuario} ejecutó el script pero salió.")
+    sys.exit()
+
+print("Esto nunca debería imprimirse")
